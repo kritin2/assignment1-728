8b15e9096a539812efbd43aea9f8e76216ed31d0
Kritin Garg <kritingarg0209@gmail.com>
2021-03-10 21:51:53 +0530

    added tokenizer

--- prepositionClass.py
+++ prepositionClass.py
@@ -1,4 +1,9 @@
 from preprocessing import readXml
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
+import re
+import nltk
+nltk.download('punkt')
 
 class PrepositionClass:
     def __init__(self, senseId, id, prepositionValue, leftContext, rightContext):
@@ -8,6 +13,26 @@ class PrepositionClass:
         self.leftContext = leftContext
         self.rightContext = rightContext
 
+    def print(self):
+        print('id : ', self.id)
+        print('senseId : ',self.senseId)
+        print(self.leftContext + "\n" + self.prepositionValue + "\n" + self.rightContext )
 
-# TODO add tokenizer for contexts    
+    
+    def tokenizeSentence(self):
+        leftContextTemp = self.leftContext.lower()
+        leftContextTemp = re.sub(r'[^\w\s]', '', leftContextTemp)
+        leftContextToken = word_tokenize(leftContextTemp)
+        self.leftContext = [word for word in leftContextToken if not word in stopwords.words()]
 
+        rightContextTemp = self.rightContext.lower()
+        rightContextTemp = re.sub(r'[^\w\s]', '', rightContextTemp)
+        rightContextToken = word_tokenize(rightContextTemp)
+        self.rightContext = [word for word in rightContextToken if not word in stopwords.words()]    
+        
+
+if __name__ == '__main__' :
+    testObject = PrepositionClass('2(1)', 'aboutid','about','He saw Owen redden with pleasure , and laughed , flinging an arm', 
+    'his shoulders so forgetfully that it was a worthy as well as a willing sacrifice Owen made for him , containing the pain of the embrace .')
+    testObject.tokenizeSentence()
+    testObject.print()
